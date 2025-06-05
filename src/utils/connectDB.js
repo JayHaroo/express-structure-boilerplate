dd9de356fd5e834bf1e3edeacaeda73d1918834d
JayHaroo <saturnomjey09@gmail.com>
2025-06-05 09:42:58 +0800

    Refactor connectDB utility to improve MongoDB connection handling

--- src/utils/connectDB.js
+++ src/utils/connectDB.js
@@ -1,4 +1,23 @@
 const { MongoClient, ObjectId } = require('mongodb');
 
 //change to env or connection string
-const client  = new MongoClient('mongodb://localhost:27017')
+const client  = new MongoClient('mongodb://localhost:27017');
+
+
+//Set the collection name
+let nameCollection;
+
+
+exports.connectDB = async () => {
+    try {
+        // Connect to the MongoDB server
+        await client.connect();
+        console.log('Connected to MongoDB');
+
+        // Select the database and collection
+        const db = client.db('userDB');
+        nameCollection = db.collection('users');
+    } catch (error) {
+        console.error('Error connecting to MongoDB:', error);
+    }
+}
